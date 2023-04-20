70d5eb9ed4ad9d4ffda3b1c89a511ed51e48eb1f
MSF2141 <114106019+MSF2141@users.noreply.github.com>
2023-04-20 17:30:04 -0400

    updated

--- charts.js
+++ charts.js
@@ -54,18 +54,19 @@ function buildMetadata(sample) {
   });
 }
 
+
 // Deliverable 1: 1. Create the buildChart function.
 function buildCharts(sample) {
   // Deliverable 1: 2. Use d3.json to load the samples.json file 
   d3.json("samples.json").then((data) => {
-    console.log(data);
-
     // Deliverable 1: 3. Create a variable that holds the samples array. 
-
+    var charts = data.charts;
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    
+   
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
-
+    
+   
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
 
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
@@ -82,27 +83,42 @@ function buildCharts(sample) {
 
     // Deliverable 1: 8. Create the trace for the bar chart. 
     var barData = [
+      x: [sample_values],
+      y: [otu_ids],
+      type: "bar"
 
     ];
 
     // Deliverable 1: 9. Create the layout for the bar chart. 
     var barLayout = {
-
+      title: "Top 10 Bacteria Cultures Found "
     };
 
     // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar", barData, barLayout);
 
     // Deliverable 2: 1. Create the trace for the bubble chart.
-
+    var bubbleData = [
+      x: ['out_ids'],
+      y: ['sample_values'],
+      type: "bubble"
+    ];
     // Deliverable 2: 2. Create the layout for the bubble chart.
-
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample "
+    };
     // Deliverable 2: 3. Use Plotly to plot the data with the layout.
-    
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
     // Deliverable 3: 4. Create the trace for the gauge chart.
-    
+    var gaugeData = [
+
+    ];
     // Deliverable 3: 5. Create the layout for the gauge chart.
-
+    var gaugeLayout = [
+      title: "Belly Button Washing Frequency (Scrubs per Week) "
+    ];
     // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
-
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
